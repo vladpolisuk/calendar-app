0dab72ba2f152e90f7e0bb8e4184c692c1c5ee2a
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-02-03 16:10:31 +0300

    chore: added css fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
